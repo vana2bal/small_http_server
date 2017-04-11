39748edf4c7247e02e9827ae9de27fe19e86dba2
florian <florian@thinkpad-flo.maison.cheznous>
2017-04-11 08:43:04 +0200

    naming conventions

--- include/requesthandler.h
+++ include/requesthandler.h
@@ -1,8 +1,13 @@
 /*
- * Main service prototype 
+ * Main service prototype
  *
  * This method is the main one of this tiny server. This handle the request
  * comming from connection on c_socket. This should parse the request and
  * respond accordingly.
  */
+ #ifndef REQUEST_HANDLER_H
+ #define REQUEST_HANDLER_H
+
 int handle_request(int c_socket);
+
+#endif
