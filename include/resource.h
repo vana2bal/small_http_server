887a9f005c88f71a4fbd6e3b2ca51f6bc7d985bd
Florian Gautier <fgautier.mail@gmail.com>
2017-04-14 10:14:03 +0200

    update resource.h

--- include/resource.h
+++ include/resource.h
@@ -3,10 +3,11 @@
  * Interface to functions for returning a resource.
  *
  */
-#include "httprequest.h"
 
 #ifndef RESOURCE_H
 #define RESOURCE_H
+
+#include "httprequest.h"
 
 int return_resource (int conn, int resource, http_request * request);
 int check_resource  (http_request * request);
